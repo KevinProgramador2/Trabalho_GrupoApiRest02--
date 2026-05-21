6d33bd24488263e031ed5e2258661066f0b8b5c0
llcardos <137669141+llcardos@users.noreply.github.com>
2026-05-20 23:36:04 -0300

    fix: remove lombok and create lancamentoVenda

--- src/main/java/br/serratec/com/trabalhovalendonota02/model/Vendedor.java
+++ src/main/java/br/serratec/com/trabalhovalendonota02/model/Vendedor.java
@@ -1,26 +1,64 @@
 package br.serratec.com.trabalhovalendonota02.model;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import java.util.List;
 
-@Getter
-@Setter
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
 @Entity
 public class Vendedor {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nome;
-    private String email;git
+    private String email;
     private Double salario;
 
-    @OneToMany
-    JoinColumn(mappedBy ="id_vAutonomo");
+    @OneToMany(mappedBy = "vendedor")
+    private List<LancamentoVendas> lancamentoVendas;
 
-    private VendedorAutonomo vendedorAutonomo;
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Double salario) {
+        this.salario = salario;
+    }
+
+    public List<LancamentoVendas> getLancamentoVendas() {
+        return lancamentoVendas;
+    }
+
+    public void setLancamentoVendas(List<LancamentoVendas> lancamentoVendas) {
+        this.lancamentoVendas = lancamentoVendas;
+    }
 }
